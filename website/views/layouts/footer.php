8eb6c846e2b53ae20efa24ce46b575f5b0456e2e
Fiqram94 <iqramrafien@gmail.com>
2021-06-17 23:45:09 +0800

    website

---
+++ website/views/layouts/footer.php
@@ -0,0 +1,48 @@
+<footer id="footer" role="contentinfo">
+        <div class="footer-inner" id="yui_3_17_2_1_1617087493649_247">
+          <div class="nav-wrapper back-to-top-nav"><nav><div class="back-to-top"><a href="https://www.bisnest.my/#header">Back to Top</a></div></nav></div>
+          
+
+
+          <div id="siteInfo"><a href="mailto:info@bisnest.my" class="site-email"><span>info@bisnest.my</span></a></div>
+          <div class="sqs-layout sqs-grid-12 columns-12" data-layout-label="Footer Content" data-type="block-field" data-updated-on="1611786550099" id="footerBlocks"><div class="row sqs-row" id="yui_3_17_2_1_1617087493649_246"><div class="col sqs-col-12 span-12" id="yui_3_17_2_1_1617087493649_245"><div class="sqs-block html-block sqs-block-html" data-block-type="2" id="block-yui_3_17_2_1_1531414292087_59034"><div class="sqs-block-content"><p style="text-align:center;white-space:pre-wrap;" class=""><a href="https://www.bisnest.my/home">Home</a>&nbsp;|&nbsp;<a href="https://www.bisnest.my/programs">About</a>&nbsp;|&nbsp;<a href="https://www.bisnest.my/team">Portfolio</a>&nbsp;|&nbsp;<a href="https://www.bisnest.my/portfolio">Blog</a></p><p style="text-align:center;white-space:pre-wrap;" class="">&nbsp;</p></div></div><div class="row sqs-row" id="yui_3_17_2_1_1617087493649_244"><div class="col sqs-col-4 span-4"><div class="sqs-block spacer-block sqs-block-spacer sized vsize-1" data-block-type="21" id="block-yui_3_17_2_1_1532555004579_10259"><div class="sqs-block-content">&nbsp;</div></div></div><div class="col sqs-col-4 span-4" id="yui_3_17_2_1_1617087493649_243"><div class="sqs-block image-block sqs-block-image sqs-text-ready" data-block-type="5" id="block-yui_3_17_2_1_1532555004579_9047"><div class="sqs-block-content" id="yui_3_17_2_1_1617087493649_242">
+
+  
+    <div class="image-block-outer-wrapper
+          layout-caption-hidden
+          design-layout-inline
+          combination-animation-none
+          individual-animation-none
+          individual-text-animation-none
+         sqs-narrow-width" data-test="image-block-inline-outer-wrapper" id="yui_3_17_2_1_1617087493649_241">
+
+      
+
+      
+        <figure class="
+              sqs-block-image-figure
+              intrinsic
+            " style="max-width:2500px;" id="yui_3_17_2_1_1617087493649_240">
+
+        <img src="<?= $dirAssests?>/images/logo.png" alt="bisnest_logo" />
+
+        </figure>
+      
+
+    </div>
+
+</div></div></div>
+
+<div class="col sqs-col-4 span-4"><div class="sqs-block spacer-block sqs-block-spacer sized vsize-1" data-block-type="21" id="block-yui_3_17_2_1_1532555004579_11398">
+
+<div class="sqs-block-content">&nbsp;</div></div></div></div>
+
+<div class="sqs-block html-block sqs-block-html" data-block-type="2" id="block-yui_3_17_2_1_1531414292087_28418">
+
+<div class="sqs-block-content"><p style="text-align:center;white-space:pre-wrap;" class=""><br><strong>BISNEST</strong><br>Kota Bharu<br>Malaysia</p><p style="text-align:center;white-space:pre-wrap;" class=""><a href="http://info@bisnest.my/">info@bisnest.my</a></p></div></div>
+
+<div class="sqs-block horizontalrule-block sqs-block-horizontalrule" data-block-type="47" id="block-yui_3_17_2_1_1579564975439_14532"><div class="sqs-block-content"><hr></div></div>
+
+<div class="sqs-block html-block sqs-block-html" data-block-type="2" id="block-yui_3_17_2_1_1531414292087_93888"><div class="sqs-block-content"><p style="text-align:center;white-space:pre-wrap;" class="">Copyright © 2021 bisnest.my. All rights reserved.</p></div></div></div></div></div>
+        </div>
+      </footer>
---
+++ website/views/layouts/portfolio.php
@@ -0,0 +1,116 @@
+<div class="sqs-block gallery-block sqs-block-gallery" data-block-json="{&quot;hSize&quot;:null,&quot;floatDir&quot;:null,&quot;methodOption&quot;:&quot;transient&quot;,&quot;existingGallery&quot;:null,&quot;newWindow&quot;:false,&quot;aspect-ratio&quot;:&quot;anamorphic-widescreen&quot;,&quot;aspectRatio&quot;:null,&quot;auto-crop&quot;:true,&quot;blockAnimation&quot;:&quot;none&quot;,&quot;collectionId&quot;:&quot;5c08217421c67c2d9fb270a8&quot;,&quot;design&quot;:&quot;grid&quot;,&quot;lightbox&quot;:false,&quot;padding&quot;:1,&quot;show-meta&quot;:true,&quot;show-meta-basic&quot;:true,&quot;show-meta-only-title&quot;:false,&quot;show-meta-only-description&quot;:false,&quot;square-thumbs&quot;:false,&quot;thumbnails-per-row&quot;:3,&quot;vSize&quot;:null,&quot;transientGalleryId&quot;:&quot;5c08217421c67c2d9fb270a8&quot;}" data-block-type="8" id="block-yui_3_17_2_1_1544036447190_11088"><div class="sqs-block-content">
+
+
+
+
+  
+
+  
+<center>
+  <div class="sqs-gallery sqs-gallery-design-grid" id="yui_3_17_2_1_1617087493649_300">
+          
+<div class="row sqs-row" id="yui_3_17_2_1_1617087493649_146">
+  <div class="col sqs-col-3 span-3">
+            <div class="slide sqs-gallery-design-grid-slide sqs-active-slide" data-type="image" data-animation-role="image" id="yui_3_17_2_1_1617087493649_334">
+              <div class="margin-wrapper">
+                
+                <a href="#" class="
+                    image-slide-anchor
+                    
+                    content-fit
+                  ">
+
+                  <img class="thumb-image loaded" src="<?= $dirAssests?>/images/portfolio/Oda-Bazaar-W.png" style="left: 0px; top: 0px; width: 180px; position: relative;">
+                </a>
+                
+              </div>
+            </div>
+          </div>
+          
+          <div class="col sqs-col-3 span-3">
+            <div class="slide sqs-gallery-design-grid-slide" data-type="image" data-animation-role="image" id="yui_3_17_2_1_1617087493649_335">
+              <div class="margin-wrapper">
+                
+                <a href="http://iconmc.com/" class="
+                    image-slide-anchor
+                    
+                    content-fit
+                  ">
+                  
+           
+                  <img class="thumb-image loaded" src="<?= $dirAssests?>/images/portfolio/IbnAgroFarm-W.png" style="left: 0px; top: 0px; width: 180px;position: relative;">
+                </a>
+                
+              </div>
+            </div>
+          </div>
+          
+
+          
+        
+
+      
+
+        
+
+        
+
+        
+          <div class="col sqs-col-3 span-3">
+            <div class="slide sqs-gallery-design-grid-slide" data-type="image" data-animation-role="image" id="yui_3_17_2_1_1617087493649_336">
+              <div class="margin-wrapper">
+                
+                <a role="presentation" class="
+                    image-slide-anchor
+                    
+                    content-fit
+                  ">
+                  
+         <img class="thumb-image loaded"  alt="GetLogoFile.png" data-image-resolution="200w" src="<?=$dirAssests?>/images/portfolio/AyamGoreng-W.jpg" style="left: 0px; top: 0px; width: 180px; position: relative;">
+                </a>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col sqs-col-3 span-3">
+            <div class="slide sqs-gallery-design-grid-slide" data-type="image" data-animation-role="image" id="yui_3_17_2_1_1617087493649_336">
+              <div class="margin-wrapper">
+                
+                <a role="presentation" class="
+                    image-slide-anchor
+                    
+                    content-fit
+                  ">
+                  
+         <img class="thumb-image loaded"  alt="edusage.png" data-image-resolution="200w" src="<?=$dirAssests?>/images/portfolio/edusage-W.png" style="left: 0px; top: 0px; width: 180px; position: relative;">
+                </a>
+                
+              </div>
+            </div>
+          </div>
+          
+
+          
+        
+
+      
+    
+  </div>
+
+  
+
+
+
+
+
+<style type="text/css" id="design-grid-css">
+#block-yui_3_17_2_1_1544036447190_11088 .sqs-gallery-block-grid .sqs-gallery-design-grid { margin-right: -1px; }
+#block-yui_3_17_2_1_1544036447190_11088 .sqs-gallery-block-grid .sqs-gallery-design-grid-slide .margin-wrapper { margin-right: 1px; margin-bottom: 1px; }
+</style>
+
+
+
+</div>
+</center>
+</div>
