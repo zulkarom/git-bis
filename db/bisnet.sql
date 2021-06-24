a9f35020336acd588c0e650f3bea62d4cc565fe4
Fiqram94 <iqramrafien@gmail.com>
2021-06-24 11:18:31 +0800

    db

--- db/bisnet.sql
+++ db/bisnet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 17, 2021 at 12:05 AM
+-- Generation Time: Jun 24, 2021 at 05:18 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.3.21
 
@@ -24,6 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `chat`
+--
+
+CREATE TABLE `chat` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `recipient_id` int(11) NOT NULL,
+  `user_id` int(10) UNSIGNED DEFAULT NULL,
+  `time` int(10) UNSIGNED DEFAULT NULL,
+  `rfc822` varchar(50) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `icon` varchar(255) DEFAULT NULL,
+  `message` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `chat`
+--
+
+INSERT INTO `chat` (`id`, `sender_id`, `recipient_id`, `user_id`, `time`, `rfc822`, `name`, `icon`, `message`) VALUES
+(37, 7, 9, NULL, 1624502789, 'Thu, 24 Jun 21 04:46:29 +0200', 'IQRAM RAFIEN', 'iqramrafien21@gmail.com', 'test'),
+(38, 7, 9, NULL, 1624502836, 'Thu, 24 Jun 21 04:47:16 +0200', 'IQRAM RAFIEN', 'iqramrafien21@gmail.com', 'test2'),
+(39, 7, 9, NULL, 1624503027, 'Thu, 24 Jun 21 04:50:27 +0200', 'IQRAM RAFIEN', 'iqramrafien21@gmail.com', 'test'),
+(40, 7, 9, NULL, 1624503044, 'Thu, 24 Jun 21 04:50:44 +0200', 'IQRAM RAFIEN', 'iqramrafien21@gmail.com', 'test333'),
+(41, 7, 9, NULL, 1624503299, 'Thu, 24 Jun 21 04:54:59 +0200', 'IQRAM RAFIEN', 'iqramrafien21@gmail.com', 'fghfgh'),
+(42, 7, 9, NULL, 1624503303, 'Thu, 24 Jun 21 04:55:03 +0200', 'IQRAM RAFIEN', 'iqramrafien21@gmail.com', 'dfgdf');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `client`
 --
 
@@ -32,6 +62,13 @@ CREATE TABLE `client` (
   `user_id` int(11) NOT NULL,
   `client_type` tinyint(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `client`
+--
+
+INSERT INTO `client` (`id`, `user_id`, `client_type`) VALUES
+(5, 7, 0);
 
 -- --------------------------------------------------------
 
@@ -45,6 +82,13 @@ CREATE TABLE `client_exper` (
   `expert_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `client_exper`
+--
+
+INSERT INTO `client_exper` (`id`, `client_id`, `expert_id`) VALUES
+(1, 5, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -56,6 +100,13 @@ CREATE TABLE `expert` (
   `user_id` int(11) NOT NULL,
   `expert_type` tinyint(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `expert`
+--
+
+INSERT INTO `expert` (`id`, `user_id`, `expert_type`) VALUES
+(1, 9, 20);
 
 -- --------------------------------------------------------
 
@@ -75,10 +126,48 @@ CREATE TABLE `migration` (
 INSERT INTO `migration` (`version`, `apply_time`) VALUES
 ('m000000_000000_base', 1608541238),
 ('m130524_201442_init', 1608541245),
+('m140209_132017_init', 1624370530),
+('m140403_174025_create_account_table', 1624370532),
+('m140504_113157_update_tables', 1624370533),
+('m140504_130429_create_token_table', 1624370535),
+('m140830_171933_fix_ip_field', 1624370536),
+('m140830_172703_change_account_table_name', 1624370537),
+('m141222_110026_update_ip_field', 1624370538),
+('m141222_135246_alter_username_length', 1624370538),
+('m150614_103145_update_social_account_table', 1624370539),
+('m150623_212711_fix_username_notnull', 1624370539),
+('m151218_234654_add_timezone_to_profile', 1624370539),
+('m160929_103127_add_last_login_at_to_user_table', 1624370540),
 ('m190124_110200_add_verification_token_column_to_user_table', 1608541245),
 ('m201221_083220_why_choose', 1608541584),
 ('m201222_023501_who_we_are', 1608604659),
 ('m201222_083600_header', 1608626268);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `public_email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gravatar_email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gravatar_id` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `location` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `website` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bio` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `timezone` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`user_id`, `name`, `public_email`, `gravatar_email`, `gravatar_id`, `location`, `website`, `bio`, `timezone`) VALUES
+(7, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(9, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -94,28 +183,75 @@ CREATE TABLE `service` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `social_account`
+--
+
+CREATE TABLE `social_account` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `provider` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `client_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `data` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `code` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `token`
+--
+
+CREATE TABLE `token` (
+  `user_id` int(11) NOT NULL,
+  `code` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `type` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `token`
+--
+
+INSERT INTO `token` (`user_id`, `code`, `created_at`, `type`) VALUES
+(9, 'HPUsm40ofwKUJScvPL23ya2WOWSdRrGU', 1624484446, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `fullname` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `status` smallint(6) NOT NULL DEFAULT 10,
+  `role` tinyint(1) NOT NULL,
+  `password_hash` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `confirmed_at` int(11) DEFAULT NULL,
+  `unconfirmed_email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `blocked_at` int(11) DEFAULT NULL,
+  `registration_ip` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
-  `verification_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+  `flags` int(11) NOT NULL DEFAULT 0,
+  `last_login_at` int(11) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`, `verification_token`) VALUES
-(1, 'superadmin', 'Hq1g4zdHntXQVsYgMDq9otLkdK-951eu', '$2y$10$3JXrEBM6ZZc9hKG7lU1wg.JgLKsvUpi6EyAAj.VL07dgIcqmFVO5m', NULL, 'iqramrafien21@gmail.com', 10, 1604212430, 1604212430, '6QfFZiOtOwD7UdJnrzVYdat6-ml9k6vY_1604212430');
+INSERT INTO `user` (`id`, `username`, `fullname`, `email`, `role`, `password_hash`, `auth_key`, `confirmed_at`, `unconfirmed_email`, `blocked_at`, `registration_ip`, `created_at`, `updated_at`, `flags`, `last_login_at`, `status`, `password_reset_token`) VALUES
+(7, 'iqramrafien21@gmail.com', 'IQRAM RAFIEN', 'iqramrafien21@gmail.com', 1, '$2y$10$ROS1EaY6SHxLLJhzgQFn3Oum9zbjt/.o42fofZCW7LOyW75zbEY96', '_qKjxhFAFD_HQ4PfyY9VdqYNVRmFTN0j', 1624467684, NULL, NULL, '::1', 1624467474, 1624467474, 0, 1624477918, 10, ''),
+(8, 'superadmin', 'Superadmin', '', 0, '$2y$10$G2CqfuUqiTshvYmzFbh/seDgLVXbHRvUrb8fu.8UxCHgyaF9vd3pG', '', NULL, NULL, NULL, NULL, 0, 0, 0, NULL, 10, ''),
+(9, 'iqramrafien@gmail.com', 'Fakhrul Iqram', 'iqramrafien@gmail.com', 2, '$2y$10$goxdKCZQPMIZlylAv.B26O9cvfEiS57quDyo.l0upvVpQzQR97F3i', '4INfZI_L_M2RuMxQEYDbfVDKtIwDNiPe', NULL, NULL, NULL, '::1', 1624484446, 1624484446, 0, 1624484468, 10, '');
 
 -- --------------------------------------------------------
 
@@ -136,7 +272,7 @@ CREATE TABLE `web_intro` (
 --
 
 INSERT INTO `web_intro` (`id`, `title`, `title_content`, `title_button`, `intro_content`) VALUES
-(1, 'BISNEST', 'ONLINE INCUBATION PLATFORM', 'LEARN MORE', 'BISNEST an online incubation platform for entrepreneurs who are looking for a starting-point for their business. The entrepreneurs are able to transform indigenous ideas into a rapidly growing companies providing products and services to the market via our online incubation platform.');
+(1, 'BISNEST', 'ONLINE INCUBATION PLATFORM', 'LEARN MOREee', 'BISNEST an online incubation platform for entrepreneurs who are looking for a starting-point for their business. The entrepreneurs are able to transform indigenous ideas into a rapidly growing companies providing products and services to the market via our online incubation platform.');
 
 -- --------------------------------------------------------
 
@@ -146,8 +282,39 @@ INSERT INTO `web_intro` (`id`, `title`, `title_content`, `title_button`, `intro_
 
 CREATE TABLE `web_portfolio` (
   `id` int(11) NOT NULL,
-  `image_url` varchar(255) NOT NULL
+  `image_url` varchar(255) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `web_portfolio`
+--
+
+INSERT INTO `web_portfolio` (`id`, `image_url`) VALUES
+(1, 'Doa-Tidak-Makbul.jpg'),
+(2, 'Doa-Tidak-Makbul.jpg'),
+(3, 'images.png'),
+(4, 'Doa-Tidak-Makbul.jpg'),
+(5, 'Doa-Tidak-Makbul.jpg'),
+(6, 'Doa-Tidak-Makbul.jpg'),
+(7, '20-0-75.71-100'),
+(8, '0-0-55.71-100'),
+(9, '19.56-0-75.27-100'),
+(10, '22.22-0-77.94-100'),
+(11, ''),
+(12, ''),
+(13, ''),
+(14, ''),
+(15, '18.44-0-74.16-100'),
+(16, '17.78-0-73.49-100'),
+(17, '0-0-55.71-100'),
+(18, '0-0-55.71-100'),
+(19, '17.11-0-72.83-100'),
+(20, '16.44-0-72.16-100'),
+(21, '20.89-0-76.6-100'),
+(22, '19.33-0-75.05-100'),
+(23, '20-0-75.71-100'),
+(24, '20.89-0-76.6-100'),
+(25, '20.89-0-76.6-100');
 
 -- --------------------------------------------------------
 
@@ -167,13 +334,19 @@ CREATE TABLE `web_section` (
 --
 
 INSERT INTO `web_section` (`id`, `title`, `content`, `image_url`) VALUES
-(1, 'STARTUPS', 'Bisnest assist entrepreneurs build companies from scratch and help start-up to grow and consolidate their business.', 'pic1.jpeg'),
+(1, 'STARTUPS', 'Bisnest assist entrepreneurs build companies from scratch and help start-up to grow and consolidate their business.', ''),
 (2, 'INVESTORS', 'Bisnest connect with investment opportunities in Malaysia.', 'pic2.jpeg'),
 (3, 'ECOSYSTEM', ' Bisnest create an entrepreneurial ecosystem focused on growing and sustaining the business and community.', 'pic3.jpeg');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `client`
@@ -200,19 +373,39 @@ ALTER TABLE `migration`
   ADD PRIMARY KEY (`version`);
 
 --
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- Indexes for table `service`
 --
 ALTER TABLE `service`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `social_account`
+--
+ALTER TABLE `social_account`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `account_unique` (`provider`,`client_id`),
+  ADD UNIQUE KEY `account_unique_code` (`code`),
+  ADD KEY `fk_user_account` (`user_id`);
+
+--
+-- Indexes for table `token`
+--
+ALTER TABLE `token`
+  ADD UNIQUE KEY `token_unique` (`user_id`,`code`,`type`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `password_reset_token` (`password_reset_token`);
+  ADD UNIQUE KEY `user_unique_username` (`username`),
+  ADD UNIQUE KEY `user_unique_email` (`email`);
 
 --
 -- Indexes for table `web_intro`
@@ -237,22 +430,28 @@ ALTER TABLE `web_section`
 --
 
 --
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
 -- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `client_exper`
 --
 ALTER TABLE `client_exper`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `expert`
 --
 ALTER TABLE `expert`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `service`
@@ -261,10 +460,16 @@ ALTER TABLE `service`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `social_account`
+--
+ALTER TABLE `social_account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `web_intro`
@@ -276,13 +481,35 @@ ALTER TABLE `web_intro`
 -- AUTO_INCREMENT for table `web_portfolio`
 --
 ALTER TABLE `web_portfolio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `web_section`
 --
 ALTER TABLE `web_section`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `fk_user_profile` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `social_account`
+--
+ALTER TABLE `social_account`
+  ADD CONSTRAINT `fk_user_account` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `token`
+--
+ALTER TABLE `token`
+  ADD CONSTRAINT `fk_user_token` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
