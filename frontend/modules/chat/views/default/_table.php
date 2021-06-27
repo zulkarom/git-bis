d24039aa2f96509ea393121ad23fee1921c2e05b
Fiqram94 <iqramrafien@gmail.com>
2021-06-27 09:05:11 +0800

    chat

--- frontend/modules/chat/views/default/_table.php
+++ frontend/modules/chat/views/default/_table.php
@@ -1,26 +1,22 @@
-<div class="card">
-    <div class="card-body">
-        <div class="chat-rbox">
-            <ul class="chat-list p-20">
-                <?php foreach ($messages as $message) : ?>
-                <!--chat Row -->
-              
-                <li>
-                    <div class="chat-content">
-                        <div class="box bg-light-info"><?= $message['message'] ?>&nbsp <font size="1"><?=date("h:i: A", strtotime($message['rfc822']));?></font></div>
-                    </div>
-                </li>
-                <!--chat Row -->
-           
-                <!--chat Row -->
-                <li class="reverse">
-                    <div class="chat-content">
-                        <div class="box bg-light-inverse"><?= $message['message'] ?>&nbsp <font size="1"><?=date("h:i: A", strtotime($message['rfc822']));?></font></div>
-                    </div>
-                </li>
-                <?php endforeach; ?>
-                <!--chat Row -->
-            </ul>
-        </div>
-    </div>
+<div class="chat-rbox">
+    <ul class="chat-list p-20">
+        <?php foreach ($messages as $message) : ?>
+        <!--chat Row -->
+      
+        <li>
+            <div class="chat-content">
+                <div class="box bg-light-info"><?= $message['message'] ?>&nbsp <font size="1"><?=date("h:i: A", strtotime($message['rfc822']));?></font></div>
+            </div>
+        </li>
+        <!--chat Row -->
+   
+        <!--chat Row -->
+        <li class="reverse">
+            <div class="chat-content">
+                <div class="box bg-light-inverse"><?= $message['message'] ?>&nbsp <font size="1"><?=date("h:i: A", strtotime($message['rfc822']));?></font></div>
+            </div>
+        </li>
+        <?php endforeach; ?>
+        <!--chat Row -->
+    </ul>
 </div>
