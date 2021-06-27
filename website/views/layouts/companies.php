072e6efb4c9edba6a80c15ab3f618164d48a151b
zulkarom <zulkarom@gmail.com>
2021-06-27 13:43:57 +0800

    t

--- website/views/layouts/companies.php
+++ website/views/layouts/companies.php
@@ -9,14 +9,14 @@
 
           
             <div class="companies-wrapper">
-              <!-- <div class="grid-containerCol w-col-3 w-col-stack">
+               <div class="grid-containerCol w-col-3 w-col-stack">
                      <div class="company-item">
                         <a data-modal-id="agencasa" target="_blank" href="https://www.facebook.com/bisnestofficial" class="company-link w-inline-block">
                            <img src="<?= $dirAssests?>/pictures/Bisnest-W.png" alt="" class="image-3 companies-image--black">
                            <img src="<?= $dirAssests?>/pictures/Bisnest-W1.png" alt="" class="image-2">
                         </a>
                      </div>
-                   </div> -->
+                   </div>
                    
                   
                    <div class="grid-containerCol w-col-3 w-col-stack img-container">
@@ -30,14 +30,14 @@
                  
         
                
-               <!-- <div class="grid-containerCol w-col-3 w-col-stack">
+                <div class="grid-containerCol w-col-3 w-col-stack">
                      <div class="company-item">
                         <a data-modal-id="agencasa" target="_blank" href="https://confvalley.com/" class="company-link w-inline-block">
                            <img src="<?= $dirAssests?>/pictures/convalley-W.png" alt="" class="image-3 companies-image--black">
                            <img src="<?= $dirAssests?>/pictures/convalley-W1.png" alt="" class="image-2">
                         </a>
                      </div>
-                   </div> -->
+                   </div> 
                   
                    
                  <div class="grid-containerCol w-col-3 w-col-stack">
@@ -71,15 +71,15 @@
                      </div>
                    </div> -->
 
-                   <!-- <div class="grid-containerCol w-col-3 w-col-stack">
+                    <div class="grid-containerCol w-col-3 w-col-stack">
                      <div class="company-item">
                         <a data-modal-id="agencasa" target="_blank" href="https://instagram.com/projekmakanmayu?igshid=1i1kdi4hglcuv" class="company-link w-inline-block">
                            <img src="<?= $dirAssests?>/pictures/projectMakanMayu-W.png" alt="" class="image-3 companies-image--black">
                            <img src="<?= $dirAssests?>/pictures/projectMakanMayu-W1.png" alt="" class="image-2">
                         </a>
                      </div>
-                   </div> -->
-<!-- 
+                   </div> 
+ 
                    <div class="grid-containerCol w-col-3 w-col-stack">
                      <div class="company-item">
                         <a data-modal-id="agencasa" target="_blank" href="https://ijeob.com/" class="company-link w-inline-block">
@@ -87,7 +87,7 @@
                            <img src="<?= $dirAssests?>/pictures/ijeob-W1.png" alt="" class="image-2">
                         </a>
                      </div>
-                   </div> -->
+                   </div> 
                    <div class="grid-containerCol w-col-3 w-col-stack">
                      <div class="company-item">
                         <a data-modal-id="agencasa" target="_blank" href="#" class="company-link w-inline-block">
