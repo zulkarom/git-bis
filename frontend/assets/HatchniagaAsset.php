6160f2fc6784f64b6a45e14b5ae592230020f7aa
Fiqram94 <iqramrafien@gmail.com>
2022-06-05 11:42:08 +0800

    fix

--- frontend/assets/HatchniagaAsset.php
+++ frontend/assets/HatchniagaAsset.php
@@ -18,6 +18,7 @@ class HatchniagaAsset extends AssetBundle
         'assets/css/theme.css',        
     ];
 
+
     public $js = [
         
         
@@ -25,7 +26,7 @@ class HatchniagaAsset extends AssetBundle
         'assets/js/scripts.js',
         'assets/js/charts/gd-analytics.js',
         'assets/js/libs/jqvmap.js',
-        'assets/js/apps/chats.js?r='.rand(100,1000),
+        'assets/js/apps/chats.js?r=123',
     ];
 
     public $depends = [
