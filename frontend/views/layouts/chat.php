b62145dabaf868dbfcfbd2c100cd988ef3dc14a5
zulkarom <zulkarom@gmail.com>
2022-06-23 11:38:51 +0800

    s

--- frontend/views/layouts/chat.php
+++ frontend/views/layouts/chat.php
@@ -31,7 +31,7 @@ $dirAssests = Yii::$app->assetManager->getPublishedUrl('@frontend/assets/hatchni
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="A powerful and conceptual apps base dashboard template that especially build for developers and programmers.">
     <!-- Fav Icon  -->
-    <link rel="shortcut icon" href="<?=$dirAssests?>/icon/favicon.png">
+    <link rel="shortcut icon" href="<?= $dirAssests?>/logo/mini-logo<?=Yii::$app->params['extension']?>.png">
     <!-- Page Title  -->
     <title><?= Html::encode($this->title) ?></title>
     <!-- StyleSheets  -->
